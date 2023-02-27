4880aba059b339c74478ae8005f78778057d652a
Nuno <nuno.carmo@mindswap.academy>
2023-02-27 11:12:45 +0000

    vercel

--- src/index.ts
+++ src/index.ts
@@ -4,13 +4,16 @@ import cors from 'cors';
 import databaseLoader from './middleware/databaseLoader';
 import createServer from './utils/server';
 dotenv.config();
+const app = createServer();
 boot();
 async function boot() {
     await mongoose.connect(process.env.MONGO_URL as string)
         .then(() => console.log('CONNECTED TO MONGO'))
         .catch((err) => console.log(err));
     await databaseLoader();
-    const app = createServer();
+
     app.use(cors());
     app.listen(process.env.PORT || 5000, () => console.log('SERVER UP'));
 }
+
+export default app;
